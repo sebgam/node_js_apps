8c995da4fb2f3c9a9ffb73402114207db01b02ac
sebas <sebastiangamboa11@gmail.com>
2017-07-17 15:43:21 -0500

    emmisors

---
+++ modulosCore/06-event-emmiter.js
@@ -0,0 +1,19 @@
+'use strict'
+
+var EventEmitter = require('events').EventEmitter,
+	pub = new EventEmitter()
+
+pub.on('myevent',function(message){
+	console.log(message)
+})	
+
+	.once('myevent',function(message){
+		console.log('se emite la primera ves: ' + message)
+	})
+
+	.emit('myevent','soy un emisor de eventos')
+
+
+pub.emit('myevent','volviendo a emitir')
+//pub.removeAllListeners('myevent')
+pub.emit('myevent','volviendo a emitir 4 ves')
