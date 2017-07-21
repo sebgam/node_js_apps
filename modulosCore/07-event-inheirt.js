71ac26773695add39fc81d1182bde9755cd0546d
sebas <sebastiangamboa11@gmail.com>
2017-07-21 13:37:47 -0500

    inheirt

--- modulosCore/07-event-inheirt.js
+++ modulosCore/07-event-inheirt.js
@@ -1,13 +1,34 @@
 'use strict'
 
-var EventEmitter = require('events').EventEmitter
+var EventEmitter = require('events').EventEmitter,
+	inherit = require('util').inherits
 
 var Clock = function(){
 	var self = this
 	setInterval(function(){
-		console.log('hola')
+		self.emit('tictac')
 	},1000)
 }
 
+inherit(Clock,EventEmitter)
+
+Clock.prototype.theTime =function(){
+
+
+	var date = new Date(),
+		hrs = date.getHours(),
+		min = date.getMinutes(),
+		sec = date.getSeconds(),
+		msg = hrs + ":"+min+ ":"+sec
+
+console.log(msg)
+
+}
+
+
 
 var cucu = new Clock()
+
+cucu.on('tictac',function(){
+	cucu.theTime()
+})
