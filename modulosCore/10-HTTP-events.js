e281eadcecb3c5d66f47549a5671660d5b72e5dc
sebas <sebastiangamboa11@gmail.com>
2017-08-01 20:12:52 -0500

    http

--- modulosCore/10-HTTP-events.js
+++ modulosCore/10-HTTP-events.js
@@ -2,9 +2,14 @@
 
 var http = require('http').createServer()
 
+function webServer(req,res)
+{
+	res.writeHead(200,{'Content-Type':'text/html'})
+	res.end('<h1>hola desde node js</h1>')
+}
 
 http
-	
+	.on('request',webServer)
 	.listen(3000,"localhost")
 
 console.log('servidor corriendo en  http://localhost:3000/')
