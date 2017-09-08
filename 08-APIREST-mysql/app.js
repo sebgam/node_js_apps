895b9b8f7ded2efdd4f3a00bba65f81c9c8bdfa8
sebas <sebastiangamboa11@gmail.com>
2017-09-08 14:39:05 -0500

    rest

---
+++ 08-APIREST-mysql/app.js
@@ -0,0 +1,27 @@
+'use strict'
+
+var express = require('express'),
+	favicon = require('serve-favicon'),
+	bodyParser = require('body-parser'),
+	morgan = require('morgan'),
+	routes = require('./route/index'),
+	faviconUrl = `${__dirname}/public/img/Temptation2.png`,
+	publicdir = express.static(`${__dirname}/public`),
+	viewDir = `${__dirname}/views`,
+	port = (process.env.PORT || 3000),
+	app = express()
+
+app
+	
+	.set('views' , viewDir)
+	.set('view engine' , 'jade')
+	.set('port', port)
+	.use(favicon(faviconUrl))
+	.use( bodyParser.json() )
+	.use( bodyParser.urlencoded({extended: false}) )
+	.use(morgan('dev'))
+	.use(publicdir)
+	.use(routes)
+
+
+module.exports = app	
